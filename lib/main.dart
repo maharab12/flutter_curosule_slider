bf1807de3c8b55a17de31212e9bb7859268d2b05
day2 <shadhintalukder190@gmail.com>
2023-10-01 12:45:56 +0600

    fetching data simply form firebase using Streambuilder

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,15 @@
+
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized(); // Ensure that Flutter is initialized.
+  WidgetsFlutterBinding
+      .ensureInitialized(); // Ensure that Flutter is initialized.
   await Firebase.initializeApp(); // Initialize Firebase.
   runApp(MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -13,48 +17,65 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Flutter Demo",
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
       home: Homepage(),
     );
   }
 }
+
 class Homepage extends StatefulWidget {
   const Homepage({super.key});
 
   @override
-  _HomepageState createState() => _HomepageState();
+  State<Homepage> createState() => _HomepageState();
 }
 
 class _HomepageState extends State<Homepage> {
-  Future getData() async {
-    var firestore = FirebaseFirestore.instance;
-    QuerySnapshot qn = await firestore.collection("country").get();
-    return qn.docs;
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blueGrey,
-      body: FutureBuilder(
-        future: getData(),
-        builder: (_, snapshot) {
-            return ListView.builder(
-              itemCount: snapshot.data?.length,
-              itemBuilder: (_, index) {
-                DocumentSnapshot  data = snapshot.data[index];
-                return Card(
-                  child: ListTile(
-                    title: Text(data["Name"]),
-                  ),
-                );
-              },
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance.collection("Catagories").snapshots(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return Center(
+                child: Text("Loading")
+            );
+          } else {
+            return GridView.builder(
+                scrollDirection: Axis.vertical,
+                itemCount: snapshot.data?.docs.length,
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2),
+                itemBuilder: (context, index) {
+                  DocumentSnapshot data = snapshot.data
+                      ?.docs[index] as DocumentSnapshot<Object?>;
+                  return Card(child: GridTile(
+                      child: Image.network(data["img"], fit: BoxFit.fill,)));
+                }
             );
           }
+        },
       ),
     );
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
